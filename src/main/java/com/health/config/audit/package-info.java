9497f3a7391202348c3690f11ea7983acad758c8
shajahan <shajahanvzm@gmail.com>
2020-04-30 16:41:05 +0530

    first commit

---
+++ src/main/java/com/health/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.health.config.audit;
---
+++ src/main/java/com/health/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.health.repository;
+
+import com.health.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
